3e11aeac332dcd0e708d91998e240665b3875f00
Oleg Sayakhov <olegsajaxov@yandex.ru>
2021-02-10 18:48:46 +0300

    miner: factorization backends interface

--- miner.py
+++ miner.py
@@ -1,10 +1,23 @@
 from sympy.ntheory import factorint
-from core import Block
+
+
+class MinerBackend:
+    MINER_BACKEND_SYMPY = 'sympy',
+
+    def __init__(self, backend):
+        self.backend = backend
+
+    def factorint(self, num):
+        if self.backend == MinerBackend.MINER_BACKEND_SYMPY:
+            return factorint(num)
+        else:
+            raise NotImplementedError()
 
 
 class Miner:
-    def __init__(self, block=None):
+    def __init__(self, backend=MinerBackend.MINER_BACKEND_SYMPY, block=None):
         self.set_block(block)
+        self.backend = MinerBackend(backend)
 
     def set_block(self, block):
         self.block = block
@@ -12,7 +25,7 @@ class Miner:
     def work(self):
         for i in range(self.block.v_diff):
             num, _ = self.block.pow.extract(i)
-            factors = factorint(num)
+            factors = self.backend.factorint(num)
 
             self.block.add_pow(num, factors)
             print(f'solved {i + 1}/{self.block.v_diff}')
